ced57444a33e712721bfb17dee9945f7a4c94f2c
TraceDecker <decker1041@gmail.com>
2020-02-15 16:48:01 -0700

    added organizer entity.

--- src/main/java/io/github/stajscavengers/scavenger/model/entity/Organizer.java
+++ src/main/java/io/github/stajscavengers/scavenger/model/entity/Organizer.java
@@ -1,5 +1,51 @@
 package io.github.stajscavengers.scavenger.model.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    indexes = {
+        @Index(columnList = "organizer_name")
+    }
+)
 public class Organizer {
 
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "organizer_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+  nullable = false, updatable = false)
+  private long id;
+
+  @NonNull
+  @Column(name = "organizer_name", nullable = false)
+  private String organizerName;
+
+// ForeignKey
+  @NonNull
+  @Column(name = "hunt_id", nullable = false, updatable = false)
+  private long huntId;
+
+  @NonNull
+  public String getOrganizerName() {
+    return organizerName;
+  }
+
+  public void setOrganizerName(@NonNull String organizerName) {
+    this.organizerName = organizerName;
+  }
+
+  public long getHuntId() {
+    return huntId;
+  }
+  
 }
