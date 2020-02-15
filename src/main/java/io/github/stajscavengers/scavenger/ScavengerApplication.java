c725b60563bb55aa6bd64727db8886d34c2c68d4
TraceDecker <decker1041@gmail.com>
2020-02-15 15:35:08 -0700

    initial commit

---
+++ src/main/java/io/github/stajscavengers/scavenger/ScavengerApplication.java
@@ -0,0 +1,13 @@
+package io.github.stajscavengers.scavenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScavengerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScavengerApplication.class, args);
+  }
+
+}
