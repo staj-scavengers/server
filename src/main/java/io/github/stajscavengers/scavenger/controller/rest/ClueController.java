9dd5375f806736424bd28a23df8c144644ed13be
TraceDecker <decker1041@gmail.com>
2020-02-27 13:46:30 -0700

    started hunt activity controller

--- src/main/java/io/github/stajscavengers/scavenger/controller/rest/ClueController.java
+++ src/main/java/io/github/stajscavengers/scavenger/controller/rest/ClueController.java
@@ -1,5 +1,13 @@
 package io.github.stajscavengers.scavenger.controller.rest;
 
+import io.github.stajscavengers.scavenger.model.entity.HuntActivity;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/huntactivity")
+@ExposesResourceFor(HuntActivity.class)
 public class ClueController {
 
 }
