2f1eb9526ab6d094711bb9fe177d301ee2286f8b
mabbyreyes <mabbyreyes@gmail.com>
2020-02-27 13:47:05 -0700

    Clue controller made and started.

--- src/main/java/io/github/stajscavengers/scavenger/controller/rest/ClueController.java
+++ src/main/java/io/github/stajscavengers/scavenger/controller/rest/ClueController.java
@@ -1,5 +1,13 @@
 package io.github.stajscavengers.scavenger.controller.rest;
 
+import io.github.stajscavengers.scavenger.model.entity.Clue;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clues")
+@ExposesResourceFor(Clue.class)
 public class ClueController {
 
 }
