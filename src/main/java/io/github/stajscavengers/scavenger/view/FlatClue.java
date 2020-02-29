d0264b2e55a861cd3e552930412fbee50265e871
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-02-29 14:51:34 -0700

    FlatCLue in progress

--- src/main/java/io/github/stajscavengers/scavenger/view/FlatClue.java
+++ src/main/java/io/github/stajscavengers/scavenger/view/FlatClue.java
@@ -1,5 +1,37 @@
 package io.github.stajscavengers.scavenger.view;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.github.stajscavengers.scavenger.model.entity.Organizer;
+import java.net.URI;
+import java.util.UUID;
+import org.springframework.lang.NonNull;
+
+@JsonPropertyOrder({"id", "clue_name", "hunt", "media", "media_tag","hunt_order"})
+
 public interface FlatClue {
 
-}
+    
+    @NonNull
+    UUID getId();
+
+    @NonNull
+    String getClueName();
+
+    @NonNull
+    Organizer getHunt();
+
+    @NonNull
+    Object getMedia();
+
+    @NonNull
+    Object getMediaTag();
+
+    @NonNull
+    Integer getHunterOrder();
+
+    @NonNull
+    URI getHref();
+
+  }
+
+
