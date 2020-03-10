f92c106cdf7e71937576b65a472334d000f4aa70
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-03-10 17:24:43 -0600

    hunt controller documentation completed

--- src/main/java/dev/staj/scavengr/controller/rest/HuntController.java
+++ src/main/java/dev/staj/scavengr/controller/rest/HuntController.java
@@ -32,6 +32,12 @@ public class HuntController {
   private final OrganizerRepository organizerRepository;
   private final ClueRepository clueRepository;
 
+  /**
+   * initializes hunt repository, organizer repository, clue repository
+   * @param huntRepository is {@link HuntRepository}
+   * @param organizerRepository
+   * @param clueRepository
+   */
   @Autowired
   public HuntController(HuntRepository huntRepository, OrganizerRepository organizerRepository,
       ClueRepository clueRepository) {
@@ -40,6 +46,11 @@ public class HuntController {
     this.clueRepository = clueRepository;
   }
 
+  /**
+   *
+   * @param hunt create new hunt
+   * @return href for hunt id.
+   */
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<Hunt> post(@RequestBody Hunt hunt) {
@@ -47,28 +58,51 @@ public class HuntController {
     return ResponseEntity.created(hunt.getHref()).body(hunt);
   }
 
+  /**
+   *
+   * @param id
+   * @return all the organizer by organizer id.
+   */
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Hunt> getByOrganizer(UUID id) {
     return huntRepository.getAllByOrganizer(id);
   }
 
+  /**
+   *
+   * @return a list of organizer.
+   */
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Hunt> getList() {
     return huntRepository.getAllByOrderByOrganizer();
   }
 
+  /**
+   * Search hunts by the keyword
+   * @param fragment
+   * @return huntName by search.
+   */
   @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Hunt> search(@RequestParam("q") String fragment) {
     return huntRepository.getAllByHuntNameContainsOrderByHuntName(fragment);
   }
 
+  /**
+   *
+   * @param id delete the hunt.
+   */
   @DeleteMapping(value = "/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable UUID id) {
     huntRepository.findById(id).ifPresent(huntRepository::delete);
   }
 
-
+  /**
+   *
+   * @param huntId  if a hunt is already assigned to an organizer so its gonna update it other wise it will create a new one
+   * @param organizerId can be assigned to huntid that doesnt have any organizer if already exist it will be updated
+   * @return
+   */
   @PutMapping(value = "/{huntId}/organizer/{organizerId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Hunt attach(@PathVariable UUID huntId, @PathVariable UUID organizerId) {
     Hunt hunt = huntRepository.findOrFail(huntId);
@@ -80,6 +114,12 @@ public class HuntController {
     return hunt;
   }
 
+  /**
+   *
+   * @param huntId if hunt id already has a hunt name update it if doesnt have any create new hunt name for that id
+   * @param huntName being assign to hunt name.
+   * @return hunt name.
+   */
   @PutMapping(value = "/{id}")
   public Hunt rename(@PathVariable UUID huntId, @RequestPart String huntName) {
     Hunt hunt = huntRepository.findOrFail(huntId);
