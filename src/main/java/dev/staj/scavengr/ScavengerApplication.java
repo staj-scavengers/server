83494485800be33ce61bbd2e6d9bb729d092130d
Samuel Martinez <semartinez147@gmail.com>
2020-03-10 15:18:20 -0600

    javadocked ScavengerApplication.

--- src/main/java/dev/staj/scavengr/ScavengerApplication.java
+++ src/main/java/dev/staj/scavengr/ScavengerApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
+/**
+ * The ScavengerApplication class provides a runnable method to initialize the server
+ */
 @EnableHypermediaSupport(type = HypermediaType.HAL)
 @SpringBootApplication
 public class ScavengerApplication {
 
+  /**
+   * The main method initializes the Scavenger server as a Spring application
+   * @param args is not used by our implementation
+   */
   public static void main(String[] args) {
     SpringApplication.run(ScavengerApplication.class, args);
   }
